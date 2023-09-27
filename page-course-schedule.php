61ca411ffa62c8574b902105372f33ac5e40b9e0
sleung204 <sally_leung18@hotmail.com>
2023-09-27 15:27:51 -0700

    got table to display

--- page-course-schedule.php
+++ page-course-schedule.php
@@ -16,43 +16,58 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-<?php
-		while ( have_posts() ) :
-			the_post();
+    <?php
+    while ( have_posts() ) :
+        the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+        get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+            comments_template();
+        endif;
 
-		endwhile; // End of the loop.
-		?>
+        // Check if the 'course_schedule' field exists for the current post.
+        if( have_rows('course_schedule') ):
+    ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Course</th>
+                        <th>Instructor</th>
+                    </tr>
+                </thead>
+                <tbody>
+    <?php
+            // Loop through rows.
+            while( have_rows('course_schedule') ) : the_row();
 
-	<?php
+                // Load sub field value.
+                $sub_date = get_sub_field('date');
+                $sub_course = get_sub_field('course');
+                $sub_instructor = get_sub_field('instructor');
+    ?>
+                <tr>
+                    <td><?php echo esc_html($sub_date); ?></td>
+                    <td><?php echo esc_html($sub_course); ?></td>
+                    <td><?php echo esc_html($sub_instructor); ?></td>
+                </tr>
+    <?php
+            // End loop.
+            endwhile;
+    ?>
+                </tbody>
+            </table>
+    <?php
+        // No value.
+        else :
+            // Handle the case where 'course_schedule' doesn't exist for this post...
+        endif;
 
-// Check rows existexists.
-if( have_rows('course_schedule') ):
-
-    // Loop through rows.
-    while( have_rows('course_schedule') ) : the_row();
-
-        // Load sub field value.
-        $sub_date = get_sub_field('date');
-		$sub_course = get_sub_field('course');
-		$sub_instructor = get_sub_field('instructor');
-        // Do something...
-echo $sub_date;
-    // End loop.
-    endwhile;
-
-// No value.
-else :
-    // Do something...
-endif;
-?>
-	</main><!-- #main -->
+    endwhile; // End of the loop.
+    ?>
+</main><!-- #main -->
 
 <?php
 get_sidebar();
