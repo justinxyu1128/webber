98ad11b066f2d74dae723386e1c910f617a09f25
Justin Yu <justinxyu1128@gmail.com>
2023-10-02 02:21:33 -0700

    added styling to 404

--- 404.php
+++ 404.php
@@ -14,10 +14,10 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'webber' ); ?></h1>
+				<h1 id="title-404" class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'webber' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<div class="page-content">
+			<div id="content-404" class="page-content">
 				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'webber' ); ?></p>
 
 					<?php
